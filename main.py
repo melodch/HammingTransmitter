65cc6d44adf16f846ddc5fdc945ba6fc2bdca857
Adi <aramachandran@olin.edu>
2021-12-06 15:03:52 -0500

    gen & parity matrices in

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+H = np.asarray_chkfinite([
+[0,0,0,1,1,1,1],
+[0,1,1,0,0,1,1],
+[1,0,1,0,1,0,1],
+],) 
+# dtype=bool)
+
+G = np.asarray_chkfinite([
+    [1,1,1,0,0,0,0],
+    [1,0,0,1,1,0,0],
+    [0,1,0,1,0,1,0],
+    [1,1,0,1,0,0,1],
+    ], )
+    # dtype=bool)
+
+
+print(G)
+print(H)
+print(np.matmul(G,np.transpose(H)))
+
+
