28700ae9444a6ec915cfd34cced00295b6e39059
Adi <aramachandran@olin.edu>
2021-12-06 15:45:33 -0500

    image edit works

--- Tx.py
+++ Tx.py
@@ -1,1 +1,13 @@
+# image
+from PIL import Image
 
+im = Image.open('tcp.png')
+img = im.load()
+
+print('before')
+print(img[500,600])
+img[500,600] = (0,0,0)
+print('after')
+print(img[500,600])
+# does this update? 
+im.save('tcp_updated.png')  # Save the modified pixels as .png
