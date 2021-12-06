8bf8cd58c0505149f9b6ff1dbe418f6a072789d2
melodch <cchiu@olin.edu>
2021-12-06 15:30:09 -0500

    add simple functions for encoding and calculating syndrome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np 
+import numpy as np
+
 
 H = np.asarray_chkfinite([
 [0,0,0,1,1,1,1],
@@ -15,9 +16,38 @@ G = np.asarray_chkfinite([
     ], )
     # dtype=bool)
 
+def convert_message_to_codeword(message):
+    """
+    Convert a 4-bit message into an 8-bit codeword.
+    """
+    return np.matmul(message, G)
 
-print(G)
-print(H)
-print(np.matmul(G,np.transpose(H)))
+def insert_error_to_codeword(codeword, error_index):
+    """
+    Insert error into codeword at given index pose.
+    """
+    error_index -= 1
+    codeword[error_index] = (codeword[error_index] + 1) % 2
+    return codeword
 
+def calculate_syndrome(codeword):
+    """
+    Calculate syndrome of given codeword.
+    """
+    syndrome = np.matmul(codeword,np.transpose(H))
+    for i in range(len(syndrome)):
+        syndrome[i] = syndrome[i] % 2
+    return syndrome
 
+print("G: ", G)
+print("H: ", H)
+# print(np.matmul(G,np.transpose(H)))
+
+message = np.asarray_chkfinite([0,0,0,1])
+print("message: ", message)
+codeword = convert_message_to_codeword(message)
+print("codeword: ", codeword)
+codeword = insert_error_to_codeword(codeword, 4)
+print("codeword with error: ", codeword)
+syndrome = calculate_syndrome(codeword)
+print("syndrome: ", syndrome)
