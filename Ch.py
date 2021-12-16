dd447d5d464162259065963887b208500c2b444e
Simrun Mutha <smutha@olin.edu>
2021-12-15 19:15:42 -0500

    channel

--- Ch.py
+++ Ch.py
@@ -2,27 +2,27 @@ import numpy as np
 import random
 
 
-def finding_bit_prob_error(total_p):
+def finding_bit_prob_error():
     """Finding bit probability error given we want 1 error in 4 bits
         Look at prob_math.jpg"""
     p = 1 - 0.97854
     return p
 
-def channel(seven_bit, bit_prop_error):
+def channel(bit, bit_prop_error):
     """ Use probability to indroduce one error
     
         Args: 
-            half_byte: 7 bit list  
+            half_byte:  bit list  
             bit_prop_error: bit probability error which between 0 adn 1
         Returns : 7 bit number with one error introduced
     """    
     # Flipping the bits with the calculated probability
     error_bit = []
-    for i in range(len(seven_bit)):
+    for i in range(len(bit)):
         if random.random() <= bit_prop_error:
-            error_bit.append((seven_bit[i] + 1) % 2) 
+            error_bit.append((bit[i] + 1) % 2) 
         else:
-            error_bit.append(seven_bit[i])
+            error_bit.append(bit[i])
     return error_bit
 
 def rand_key(p):
@@ -51,7 +51,7 @@ num_errors = 0
 num_seven_bits = 200
 for _ in range(num_seven_bits):
     og_bit = rand_key(7)
-    error_bit = channel(og_bit, finding_bit_prob_error(0.996))
+    error_bit = channel(og_bit, finding_bit_prob_error())
     num_errors += check_error(og_bit, error_bit)
     print("Original bit: ", og_bit, " Error bit: ", error_bit)
 print("Number of bits with an error: ", num_errors, "for ", num_seven_bits)
