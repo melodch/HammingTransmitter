7d083dc311f4a0a3e669defe3cb2956565443c7b
SeunguLyu <42980939+SeunguLyu@users.noreply.github.com>
2021-12-15 20:44:49 -0500

    Add even more images

--- main.py
+++ main.py
@@ -34,7 +34,36 @@ messages = [[0,0,0,1],
             [1,0,0,0],
             [1,0,1,1]]
 
+def check_hamming():
+    ham_decode_time_taken = []
+    for message in messages:
+        # Encode
+        message = np.asarray_chkfinite(message)
+        print(message, " "*6, "message")
+        codeword = HamTx.convert_message_to_codeword(message, G)
+        print(codeword, " codeword")
+
+        # Insert error
+        error_pose = 2
+        codeword = HamTx.insert_error_to_codeword(codeword, error_pose)
+        print(codeword, " codeword with error at position", error_pose)
+
+        # Decode
+        initial_t = time.time()
+        codeword = HamRx.fix_error(codeword, H)
+        print(codeword, " codeword with fix")
+        message = HamRx.convert_codeword_to_message(codeword)
+        print(message, " "*6, "message\n")
+        ham_decode_time_taken.append(time.time() - initial_t)
+    ham_average_time_taken = sum(ham_decode_time_taken)/len(ham_decode_time_taken)
+    print("Average time taken to decode with hamming(7,4): ", ham_average_time_taken, "\n")
+
 def transfer_image_with_hamming(prop,image_name):
+    """ 
+    Transfer image through noise channel but with hamming code.
+    Final image is saved with .png format
+
+    """ 
     image_matrix = HamTx.image_to_pixels(image_name)
     for i in range(len(image_matrix)):
         for j in range(len(image_matrix[0])):
@@ -76,6 +105,11 @@ def transfer_image_with_hamming(prop,image_name):
     img.save(new_name)
 
 def transfer_image_without_hamming(prop, image_name):
+    """ 
+    Transfer image through noise channel without hamming code.
+    Final image is saved with .png format
+
+    """ 
     image_matrix = HamTx.image_to_pixels(image_name)
     for i in range(len(image_matrix)):
         for j in range(len(image_matrix[0])):
@@ -109,62 +143,8 @@ def transfer_image_without_hamming(prop, image_name):
     img.save(new_name)
 
 
-# # HAMMING
-# ham_decode_time_taken = []
-# for message in messages:
-#     # Encode
-#     message = np.asarray_chkfinite(message)
-#     print(message, " "*6, "message")
-#     codeword = HamTx.convert_message_to_codeword(message, G)
-#     print(codeword, " codeword")
-
-#     # Insert error
-#     error_pose = 2
-#     codeword = HamTx.insert_error_to_codeword(codeword, error_pose)
-#     print(codeword, " codeword with error at position", error_pose)
-
-#     # Decode
-#     initial_t = time.time()
-#     codeword = HamRx.fix_error(codeword, H)
-#     print(codeword, " codeword with fix")
-#     message = HamRx.convert_codeword_to_message(codeword)
-#     print(message, " "*6, "message\n")
-#     ham_decode_time_taken.append(time.time() - initial_t)
-# ham_average_time_taken = sum(ham_decode_time_taken)/len(ham_decode_time_taken)
-# print("Average time taken to decode with hamming(7,4): ", ham_average_time_taken, "\n")
-
-# # REPETITION
-# rep_decode_time_taken = []
-# deg_repetition = 3
-# for message in messages:
-#     # Encode
-#     message = np.asarray_chkfinite(message)
-#     print(message, " "*16, "message")
-#     codeword = RepTx.convert_message_to_codeword(message, deg_repetition)
-#     print(codeword, " codeword")
-
-#     # Insert error
-#     codeword = RepTx.insert_error_to_codeword(codeword, error_pose)
-#     print(codeword, " codeword with error at position", error_pose)
-
-#     # Decode
-#     initial_t = time.time()
-#     codeword = RepRx.fix_error(codeword, deg_repetition)
-#     print(codeword, " codeword with fix")
-#     message = RepRx.convert_codeword_to_message(codeword, deg_repetition)
-#     print(message, " "*16, "message\n")
-#     rep_decode_time_taken.append(time.time() - initial_t)
-# rep_average_time_taken = sum(rep_decode_time_taken)/len(rep_decode_time_taken)
-# print("Average time taken to decode with repition(3): ", rep_average_time_taken, "\n")
-
-# if ham_average_time_taken < rep_average_time_taken:
-#     print("Decoding with hamming code faster than repetition code by", rep_average_time_taken-ham_average_time_taken, "seconds")
-# else:
-#     print("rep faster")
-
-
 prop = 1 - 0.97854
-image_name = 'icon.jpg'
+image_name = 'octo.jpg'
 
 transfer_image_with_hamming(prop,image_name)
 transfer_image_without_hamming(prop,image_name)
